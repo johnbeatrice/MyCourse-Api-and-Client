1b9dbbf84385aecce2e18512b31f49808f6d0101
John Beatrice <81265448+johnbeatrice@users.noreply.github.com>
2023-03-03 20:41:22 -0500

    remove api url

--- client/my-course/src/pages/settings.js
+++ client/my-course/src/pages/settings.js
@@ -36,7 +36,7 @@ function Settings() {
     // make modal disappear
     setDeleteProfile(false);
     try {
-      let res = await fetch("https://MyCourseApi.johnbeatrice.repl.co/deleteUser", {
+      let res = await fetch("url", {
         method: "PUT",
         // headers and mode were required to avoid cors error
         mode: 'cors',
